d77c876fd26bccf7f45678dc59459b9c7bfb6000
edward-cheesman <edward.cheesman@gmail.com>
2024-12-04 21:38:16 +0200

    day 2 part 1 working

--- day2/part1/src/main/kotlin/org/fermented/dairy/aoc/day2/Calculator.kt
+++ day2/part1/src/main/kotlin/org/fermented/dairy/aoc/day2/Calculator.kt
@@ -1,29 +1,31 @@
 package org.fermented.dairy.aoc.day2
 
 import java.io.File
+import javax.sound.sampled.UnsupportedAudioFileException
+import kotlin.math.abs
 
 class Calculator {
-    fun calculate(filename: String): Int =
-        readFile(filename) { line ->
+    fun calculate(filename: String): Int = readFile(filename) { line ->
             line.split(" ")
-                .map{ it.toInt() }
+                .map { it.toInt() }
                 .toList()
-        }.map {
-            report ->
-            val status = if (report.size == 1) {
-                Status.SAFE
-            } else {
-                val monotonicity: Monotonicity = Monotonicity.compare(report[0], report[1])
-                for (i in 2 until report.size) {
-                    if (Monotonicity.compare(report[i - 1], report[i]) != monotonicity) {
-                        return@map Status.UNSAFE
-                    } else if (distance(report[i - 1], report[i]) > 3) {
-                        return@map Status.UNSAFE
-                    }
-                }
-                return@map Status.SAFE
-            }
-        }.count { it == Status.SAFE }
+        }.map { report ->
+            val set = report.windowed(3)
+                .map windowMap@{ window ->
+                    if (window[0] == window[1] ||
+                        abs(window[0] - window[1]) > 3 ||
+                        window[1] == window[2] ||
+                        abs(window[1] - window[2]) > 3
+                    )
+                        return@windowMap Status.UNSAFE
+                    else if (!(minOf(window.first(), window.last())..maxOf(window.first(), window.last()))
+                            .contains(window[1]))
+                        return@windowMap Status.UNSAFE
+                    else return@windowMap Status.SAFE
+                }.toSet()
+            return@map set
+        }.count { !it.contains(Status.UNSAFE) }
+
 
     private fun <T> readFile(fileName: String, builder: (String) -> T): List<T> =
         File(fileName).useLines { lines ->
